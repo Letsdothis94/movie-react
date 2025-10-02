2c2b8c114a6948af24ff2a6170ba9923f8326dcf
Cristian Contreras <contreras.cristian994@gmail.com>
2025-10-02 04:07:11 -0400

    MovieCard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Search from './components/Search'
 import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -63,7 +64,7 @@ const App = () => {
             <Search search={search} setSearch={setSearch} />
           </header>
           <section className='all-movies'>
-            <h2>All movies</h2>
+            <h2 className='mt-[40px]'>All movies</h2>
 
             {isLoading ? (
               <Spinner />
@@ -71,8 +72,8 @@ const App = () => {
               <p className='text-red-500'>{errorMessage}</p>
             ) : (
               <ul>
-                {movies.map((movie, index) => (
-                  <p key={index} className='text-white'>{movie.title}</p>
+                {movies.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )}
